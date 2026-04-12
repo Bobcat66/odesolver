25c8842f520b8c47cdea3238daaef0987344b33d
Jesse Kane <jessekane66@gmail.com>
2026-04-12 16:52:05 -0400

    finished vector field impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod vector_field;
+use core::time;
+use std::f64::consts::PI;
+
+use nalgebra::SVector;
+
 fn main() {
+    // A simple example of the ODE solver that models a pendulum
+    const MU: f64 = 0.0; // This 
+    const G: f64 = 9.81; // gravity, in m/s/s
+    const L: f64 = 1.0; // Length of the pendulum, in meters
+
+    // First element is theta
+    // Second element is theta dot
+    let pendulum_state: vector_field::VectorField<2> = vector_field::VectorField::new(
+        [
+            |point: &SVector<f64, 2>, time: f64| point[1],
+            |point: &SVector<f64, 2>, time: f64| (-MU * point[1]) - ((G/L) * point[0].sin())
+        ]
+    );
+
+    let point = SVector::<f64,2>::new(PI/2.0,0.0);
+    let point_dot = pendulum_state.eval(point,0.0);
+    println!("{:?}", point_dot);
+    // This is a state-space representation of the pendulum, which decomposes the second-order ODE describing the dynamics of the system into a pair of first-order differential equations
+    // One axis is the position of the pendulum (measured as an angle), the other axis is the velocity of the pendulum.
+    // The dynamics of the system are represented as a vector field over the state space, which maps each point of the state space to a vector describing its derivative
+
+    
     println!("Hello, world!");
 }
---
+++ src/vector_field.rs
@@ -0,0 +1,24 @@
+use nalgebra::SVector;
+pub struct VectorField<const D: usize> {
+    // Axes is an array of function pointers that accept a state and a time.
+    axes: [fn(&SVector<f64,D>,f64) -> f64; D]
+}
+
+impl<const D: usize> VectorField<D> {
+
+    pub fn new(funcs: [fn(&SVector<f64,D>,f64) -> f64; D]) -> Self {
+        Self {
+            axes: funcs
+        }
+    }
+
+    pub fn eval(&self, point: SVector<f64,D>, time: f64) -> SVector<f64,D> {
+        let mut vec: SVector<f64,D> = SVector::zeros();
+
+        for i in 0..D {
+            vec[i] = (self.axes[i])(&point,time);
+        }
+
+        vec
+    }
+}
