87efb19e59567e4cd94a351e1db7449ec34924f5
Jesse Kane <jessekane66@gmail.com>
2026-04-16 00:31:37 -0400

    removed files

--- src/solvers/runge_kutta/mod.rs
+++ src/solvers/runge_kutta/mod.rs
@@ -1,11 +1,8 @@
 // Copyright (c) Jesse Kane
 // You may use, distribute, and modify this software under the terms of
 // the license found in the root directory of this project
-mod rkimpl;
-pub mod rk_kernel;
 pub mod dopri5;
 pub mod rk_dense;
-pub mod adaptive_rk_solver;
 pub mod rk_stepper;
 pub mod adaptive_rk;
 pub mod rk_solver;
