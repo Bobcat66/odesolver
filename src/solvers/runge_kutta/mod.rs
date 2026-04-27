ff8a47c5db10c9f0223c115704d639c1c5d1d180
Jesse Kane <jessekane66@gmail.com>
2026-04-27 11:34:55 -0400

    removed old code

--- src/solvers/runge_kutta/mod.rs
+++ src/solvers/runge_kutta/mod.rs
@@ -4,11 +4,8 @@ use crate::solvers::runge_kutta::{dopri3::DOPRI3, dopri5::DOPRI5, dopri853::DOPR
 // You may use, distribute, and modify this software under the terms of
 // the license found in the root directory of this project
 pub mod dopri5;
-pub mod rk_stepper;
 pub mod adaptive_prk;
-pub mod rk_solver;
 pub mod dopri853;
-pub mod rk_method;
 pub mod dopri3;
 pub mod fixed_prk;
 pub mod prk_method;
